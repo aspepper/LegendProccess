03d39ca383b3f7a0d96df95b296650387f12fb5b
aspepper <aspepper@hotmail.com>
2024-07-31 22:11:38 -0300

    Ajustes

--- ProcessaLegendasConsole/obj/Debug/net8.0/ProcessaLegendasConsole.AssemblyInfo.cs
+++ ProcessaLegendasConsole/obj/Debug/net8.0/ProcessaLegendasConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessaLegendasConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133b4f18ecc34e2722caae806618f3d003e192f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414aee29d5b1e4747b532482743f41cb9ff9bce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessaLegendasConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessaLegendasConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
